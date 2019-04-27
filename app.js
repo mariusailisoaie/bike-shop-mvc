5ecf52326cdc08510e69dde38403ed134f58087f
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-27 15:46:48 +0200

    Set ejs as view engine and add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
