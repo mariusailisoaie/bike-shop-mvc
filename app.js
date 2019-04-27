6a61fb0012d851a2663b11c2666ff910de20c975
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-27 17:08:37 +0200

    Serve static files

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
