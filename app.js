5634d999839e778c4ac505998d6b290b08d46282
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-27 17:15:40 +0200

    Fix port for heroku

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
