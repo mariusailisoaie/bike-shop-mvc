9ef23a18b77f9523c97eb304cd5ef645256c448e
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-27 16:10:29 +0200

    Require and use admin and shop routes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
