6ecbd484cfcc2579e34092fcc6a4ef970880e32b
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-01 22:43:47 +0200

    Import and implement products controller in admin route.

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-  res.render('admin', { pageTitle: 'Add Product' });
-});
+const productsController = require('../controllers/products');
+
+router.get('/add-product', productsController.getAddBikePage);
+
+router.post('/post-bike', productsController.postBike);
 
 module.exports = router;
