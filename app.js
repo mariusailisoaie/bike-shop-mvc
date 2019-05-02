619fecbc57121d868f949ba2b201a1dea3fb77d2
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-02 20:24:59 +0200

    Add and use pageNotFoundController

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const pageNotFoundController = require('./controllers/404');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
@@ -14,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(pageNotFoundController.get404Page);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
