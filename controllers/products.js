48b7843131682ae9560177e227ed81269ead65b7
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-02 19:22:28 +0200

    Add 3 bicycles as default. Add bikeUrl for photos.

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,23 @@
-const bikes = [];
+const bikes = [
+  {
+    bikeName: 'Mountain Bike',
+    bikePrice: '$ 500',
+    bikeDescription: 'This is a very nice bike for climbing mountains.',
+    bikeUrl: 'https://images.pexels.com/photos/462036/pexels-photo-462036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=680&w=480',
+  },
+  {
+    bikeName: 'BMX',
+    bikePrice: '300 EUR',
+    bikeDescription: 'Bike for tricks in the park.',
+    bikeUrl: 'https://images.pexels.com/photos/1580234/pexels-photo-1580234.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=480',
+  },
+  {
+    bikeName: 'City Bike',
+    bikePrice: '400 EUR',
+    bikeDescription: 'Ideal for city rides.',
+    bikeUrl: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=480',
+  }
+];
 
 exports.getAddBikePage = (req, res, next) => {
   res.render('admin', { pageTitle: 'Add Product' });
@@ -9,6 +28,7 @@ exports.postBike = (req, res, next) => {
     bikeName: req.body.bikeName,
     bikePrice: req.body.bikePrice,
     bikeDescription: req.body.bikeDescription,
+    bikeUrl: req.body.bikeUrl
   });
 
   res.redirect('/');
