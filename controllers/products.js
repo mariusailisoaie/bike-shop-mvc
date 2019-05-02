1f68e87eb41897f7a123845eb4531e999c71a7dd
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-02 20:42:11 +0200

    Refactor code to use bike model. Move bikes array to bike model.

--- controllers/products.js
+++ controllers/products.js
@@ -1,40 +1,17 @@
-const bikes = [
-  {
-    bikeName: 'Mountain Bike',
-    bikePrice: '$ 500',
-    bikeDescription: 'This is a very nice bike for climbing mountains.',
-    bikeUrl: 'https://images.pexels.com/photos/462036/pexels-photo-462036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=680&w=480',
-  },
-  {
-    bikeName: 'BMX',
-    bikePrice: '300 EUR',
-    bikeDescription: 'Bike for tricks in the park.',
-    bikeUrl: 'https://images.pexels.com/photos/1580234/pexels-photo-1580234.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=480',
-  },
-  {
-    bikeName: 'City Bike',
-    bikePrice: '400 EUR',
-    bikeDescription: 'Ideal for city rides.',
-    bikeUrl: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=480',
-  }
-];
+const Bike = require('../models/bike');
 
 exports.getAddBikePage = (req, res, next) => {
   res.render('admin', { pageTitle: 'Add Product' });
 }
 
 exports.postBike = (req, res, next) => {
-  bikes.push({
-    bikeName: req.body.bikeName,
-    bikePrice: req.body.bikePrice,
-    bikeDescription: req.body.bikeDescription,
-    bikeUrl: req.body.bikeUrl
-  });
+  const { bikeName, bikePrice, bikeDescription, bikeUrl } = req.body;
+  const bike = new Bike(bikeName, bikePrice, bikeDescription, bikeUrl);
+  bike.save();
 
   res.redirect('/');
 }
 
 exports.renderBikes = (req, res, next) => {
-  console.log(bikes);
-  res.render('shop', { pageTitle: 'Shop', bikes: bikes });
+  res.render('shop', { pageTitle: 'Shop', bikes: Bike.fetchAll() });
 }
