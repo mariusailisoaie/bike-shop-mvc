82dfc417e1e2fd927b1474e0257386b7bb5e9d4d
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-03 22:54:27 +0200

    Rename products controller to bikes-controller

--- controllers/bikes-controller.js
+++ controllers/bikes-controller.js
@@ -1,7 +1,7 @@
 const Bike = require('../models/bike');
 
 exports.getAddBikePage = (req, res, next) => {
-  res.render('admin', { pageTitle: 'Add Product' });
+  res.render('admin/admin', { pageTitle: 'Add Product' });
 }
 
 exports.postBike = (req, res, next) => {
@@ -14,6 +14,6 @@ exports.postBike = (req, res, next) => {
 
 exports.renderBikes = (req, res, next) => {
   Bike.fetchAll(bikes => {
-    res.render('shop', { pageTitle: 'Shop', bikes });
+    res.render('shop/bikes-list', { pageTitle: 'Shop', bikes });
   });
 }
