8e46fb8a4bafee1c9283aa6581a861f12bdc3006
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-04 01:16:52 +0200

    Add homeController and render home page. Update bikesController.renderBikes path.

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const bikesController = require('../controllers/bikes-controller');
+const homeController = require('../controllers/home-controller');
 
-router.get('/', bikesController.renderBikes);
+router.get('/', homeController.renderHomePage);
+router.get('/bikes', bikesController.renderBikes);
 
 module.exports = router;
