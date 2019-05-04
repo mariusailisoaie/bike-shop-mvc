68069bf91d4e4aa9401a45a233cf29360fa61edd
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-05-04 16:49:32 +0200

    Add route for bike-details

--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,7 @@ const shopController = require('../controllers/shop-controller');
 
 router.get('/', shopController.renderHomePage);
 router.get('/bikes', shopController.renderBikes);
+router.get('/bikes/:bikeId', shopController.renderBikeDetails);
 router.get('/cart', shopController.renderCart);
 
 module.exports = router;
